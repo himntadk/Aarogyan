8556fe82ff5d76bece62b62cecfb3c816427d1fb
himntadk <himantabiswadeka234@gmail.com>
2023-06-20 03:10:56 +0530

    New Commit

--- DB/connectmedico.js
+++ DB/connectmedico.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/medico",{useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect("mongodb://localhost:27017/medico",{useNewUrlParser: true, useUnifiedTopology: true})
+// .then(()=> console.log('Database Connection Established!'))
+// .catch((err)=>console.log(err));
+
+var password = encodeURIComponent("#Him@nta1234");
+var user= encodeURIComponent("himantabiswadeka234");
+mongoose.set("strictQuery", false);
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.izypxdo.mongodb.net/medico?retryWrites=true&w=majority`,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log('Database Connection Established!'))
 .catch((err)=>console.log(err));
